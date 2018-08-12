19176261fe94713c6a2ecf90e647befa32d1be85
Pranav Dixit <prnvdixit@gmail.com>
2018-08-13 00:37:17 +0530

    Use Selenium-chromedriver for better headless

--- html2web-selenium.py
+++ html2web-selenium.py
@@ -1,13 +1,22 @@
 import glob
 from PIL import Image
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import os 
 
 # get a list of all the files to open
 glob_folder = os.path.join("/home/prnvdixit/Codes/Pyfont", '*.html')
 
-os.environ['MOZ_HEADLESS'] = '1'
- 
+chrome_options = Options()  
+chrome_options.add_argument("--headless")  
+chrome_options.add_argument("--window-size=%s" % "800,600")
+chrome_options.binary_location = "/usr/bin/google-chrome"
+
+driver = webdriver.Chrome(executable_path='/home/prnvdixit/Desktop/chromedriver',
+                            chrome_options=chrome_options
+                            )
+
+#os.environ['MOZ_HEADLESS'] = '1'
 #driver = webdriver.Firefox(executable_path='/home/prnvdixit/Desktop/geckodriver')
 #driver.get('http://10.14.36.185:8080/')
 #driver.save_screenshot("/home/prnvdixit/Desktop/screenshot.png")
@@ -21,9 +30,9 @@ for html_file in html_file_list:
     temp_name = "file://" + html_file
 
     # open in webpage
-    driver = webdriver.Firefox(executable_path='/home/prnvdixit/Desktop/geckodriver')
     driver.get(temp_name)
     save_name = '00' + str(index) + '.png'       
-    driver.save_screenshot("UbuntuMono-HTML.png")
-    driver.quit()
+    driver.save_screenshot(save_name)
     index += 1
+
+driver.quit()
