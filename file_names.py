0413e78e4f8fa53270bc00a8001e1a85e95e8384
Pranav Dixit <prnvdixit@gmail.com>
2018-08-23 11:37:28 +0530

    Add standard names to create/delete temp files

---
+++ file_names.py
@@ -0,0 +1,7 @@
+#!usr/bin/python3
+
+denoised_threshed_input_image = "processed-input-image.png"
+full_page_html_image = "font-html-image.png"
+cropped_html_image = "test-font-image.png"
+edited_html = "another-test.html"
+template_html = "test.html"
