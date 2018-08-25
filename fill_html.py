e38c4c72a29ebe5b8e489784c67bb838f186e6d2
Pranav Dixit <prnvdixit@gmail.com>
2018-08-25 22:41:53 +0530

    Modify compare_img and fill_html logic as well

--- fill_html.py
+++ fill_html.py
@@ -1,11 +1,18 @@
+import cv2
+
 import os
 import argparse
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+import file_names
+import crop_to_text
+import compare_img
+
 def screenshot_html(args, ocr_text):
 
-    filename = "test.html"
+    filename = file_names.template_html
 
     font_dir = args["directory"];
 
@@ -20,8 +27,7 @@ def screenshot_html(args, ocr_text):
                                 chrome_options=chrome_options
                                 )
 
-    index = 0
-
+    error_values = {}
 
     text = open(filename, "r").read()
 
@@ -32,17 +38,28 @@ def screenshot_html(args, ocr_text):
             fam_name = (dir_name)
             for file_name in files:
                 font_name = ('/'.join(path) + "/" + file_name)
-                with open("another-test.html", "w") as edit:
+                with open(file_names.edited_html, "w") as edit:
                     edit.write(text.replace("fam_name", fam_name).replace("font_name", font_name).replace("ocr_text", ocr_text))
 
-                temp_name = "file://" + "/home/prnvdixit/Codes/Pyfont/another-test.html"
+                temp_name = "file://" + "/home/prnvdixit/Codes/Pyfont/" + file_names.edited_html
 
                 driver.get(temp_name)
-                save_name = '00' + str(index) + '.png'
+                save_name = file_names.full_page_html_image
                 driver.save_screenshot(save_name)
-                index += 1
+
+                # Save cropped HTML page image
+                image = cv2.imread(save_name, cv2.IMREAD_GRAYSCALE)
+                image = crop_to_text.fit_to_text(image)
+                cv2.imwrite(file_names.cropped_html_image, image)
+
+                # Call error values
+                error_val = compare_img.mse(cv2.imread(file_names.cropped_html_image),
+                cv2.imread(file_names.denoised_threshed_input_image))
+
+                error_values[font_name] = error_val
 
     driver.quit()
+    return error_values
 
 if __name__ == "__main__":
 
